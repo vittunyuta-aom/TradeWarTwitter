7d86074d5c2dd711b9b9939bcc731ed423d76590
aommoaGitHub <vittunyuta@gmail.com>
2019-05-23 21:48:38 +0700

    finish version2

--- visualization-backend/index.js
+++ visualization-backend/index.js
@@ -1,7 +1,16 @@
 const server = require('express')()
 const Twitter = require('twitter')
 const socketIO = require('socket.io')
-const moment = require('moment')
+var firebase = require('firebase');
+var appdb = firebase.initializeApp({
+    apiKey: "AIzaSyA4A084RgNIa_8iwZJ7hTijJjzKBqAjjDU",
+    authDomain: "blackpinkapi.firebaseapp.com",
+    databaseURL: "https://blackpinkapi.firebaseio.com",
+    projectId: "blackpinkapi",
+    storageBucket: "blackpinkapi.appspot.com",
+    messagingSenderId: "925066816671",
+    appId: "1:925066816671:web:e7563c7c91b3771c"
+});
 
 const port = 4000
 const app = server.listen(port, () => {
@@ -15,23 +24,34 @@ var client = new Twitter({
     access_token_secret: '9YE7FP2tVTjpsxsgDHvJYKPfhArSRxxfR7nHb2hKHiur3'
 });
 
+
+let events = []
+appdb.database().ref('tweets').once('value', function (snapshots) {
+    if (snapshots.val()) {
+        events = Object.values(snapshots.val())
+    }
+})
+
 const io = socketIO.listen(app)
 // รอการ connect จาก client
 io.on('connection', client => {
     console.log('user connected')
-
+    io.sockets.emit('messages', events)
     // เมื่อ Client ตัดการเชื่อมต่อ
     client.on('disconnect', () => {
         console.log('user disconnected')
     })
 })
 
-const stream = client.stream('statuses/filter', { track:'#tradewar'})
+const stream = client.stream('statuses/filter', { track: '#tradewar' })
 stream.on('data', function (event) {
-    if (event){
-        console.log(event.created_at)
+    if (event) {
+        console.log(event)
         console.log('-------------------------------------------------')
-        io.sockets.emit('new-message', event )
+        // io.sockets.emit('new-message', event)
+        events.push(event)
+        io.sockets.emit('messages', events)
+        appdb.database().ref('tweets').set(events)
     }
 })
 
--- visualization/src/App.js
+++ visualization/src/App.js
@@ -1,61 +1,60 @@
 import React, { Component } from 'react'
 import socketIOClient from 'socket.io-client'
 import moment from 'moment'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, } from 'recharts';
 
 const bppink = '#f5a7b9'
-const starttime = moment().add(5, 'hours').format('MMMM Do YYYY, h:mm:ss')
+const starttime = moment().format('DD MMMM YYYY, HH:mm')
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       message: [],
-      // endpoint: "http://localhost:4000" // เชื่อมต่อไปยัง url ของ realtime server
+      // endpoint: "http://localhost:4000" // connect to backend
       endpoint: "35.225.174.2"
     }
   }
 
   componentDidMount = () => {
     const { endpoint, message } = this.state
-    const temp = message
     const socket = socketIOClient(endpoint)
-    socket.on('new-message', (messageNew) => {
-      temp.push(messageNew)
-      this.setState({ message: temp })
+    socket.on('messages', messages => {
+      this.setState({ message: messages })
     })
   }
 
   render() {
     const { message } = this.state
-    const data = [{ name: '00:00', count: 0 }, { name: '01:00', count: 0 }, { name: '02:00', count: 0 }, { name: '03:00', count: 0 },
-    { name: '04:00', count: 0 }, { name: '05:00', count: 0 }, { name: '06:00', count: 0 }, { name: '07:00', count: 0 },
-    { name: '08:00', count: 0 }, { name: '09:00', count: 0 }, { name: '10:00', count: 0 }, { name: '11:00', count: 0 },
-    { name: '12:00', count: 0 }, { name: '13:00', count: 0 }, { name: '14:00', count: 0 }, { name: '15:00', count: 0 },
-    { name: '16:00', count: 0 }, { name: '17:00', count: 0 }, { name: '18:00', count: 0 }, { name: '19:00', count: 0 },
-    { name: '20:00', count: 0 }, { name: '21:00', count: 0 }, { name: '22:00', count: 0 }, { name: '23:00', count: 0 },
-    ]
+    const data = []
     message.forEach(m => {
-      const mtime = moment(m.created_at).add(5, 'hours').format('hh')
+      const mtime = moment(m.created_at).format('DD/MM/YY HH:mm')
+      let found = false
+
+      if (data.length == 0)
+        data.push({ name: mtime, countOfEachMin: 1 })
+
       for (let i = 0; i < data.length; i++) {
-        if (data[i].name.substring(0,2) === mtime) {
-          data[i].count++
+        if (data[i].name === mtime) {
+          data[i].countOfEachMin++
+          found = true
           break
         }
       }
+      if (!found) {
+        data.push({ name: mtime, countOfEachMin: 1 })
+      }
     })
 
-    // const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-    // { name: 'Page B', uv: 200, pv: 2400, amt: 2400 }];
     return (
       <div>
         <div style={{ weight: '100%', height: '40px', backgroundColor: bppink }}>
-          <h2 style={{ color: 'black', 'font-family': 'Fira Sans', paddingTop: 6, paddingLeft: 20 }}>Start Time: {starttime} +0000
+          <h2 style={{ color: 'black', 'font-family': 'Fira Sans', paddingTop: 6, paddingLeft: 20 }}>Start Time: {starttime} +07:00
           </h2>
         </div>
         <div style={{ paddingTop: '20px' }}>
           <LineChart
-            width={1000}
+            width={1200}
             height={400}
             data={data}
             margin={{
@@ -63,25 +62,29 @@ class App extends Component {
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
+            <XAxis dataKey="name"/>
             <YAxis />
             <Tooltip />
             <Legend />
-            <Line type="monotone" dataKey="count" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="countOfEachMin" stroke={bppink} activeDot={{ r: 8 }} />
           </LineChart>
         </div>
         <div style={{ weight: '100%', height: '50px', backgroundColor: bppink }}>
           <h1 style={{ color: 'black', 'font-family': 'Fira Sans', paddingTop: 5, paddingLeft: 20 }}>Tweets contain '#tradewar'</h1>
         </div>
-        <div style={{ height: '700px', overflow: 'scroll' }}>
+        <div style={{ height: '640px', overflow: 'scroll' }}>
           {
             message.map((data, i) =>
               <div key={i} style={{ marginTop: 20, paddingLeft: 30, paddingRight: 30 }} >
-                {i + 1} : {data.created_at} : {data.text}
+                {i + 1} : {moment(data.created_at).format()} : {data.text}
               </div>
             )
           }
         </div>
+        <div style={{ weight: '100%', height: '30px', backgroundColor: bppink }}>
+          <center><h4 style={{ color: 'black', 'font-family': 'Fira Sans', paddingTop: 6, paddingLeft: 20 }}>Vittunyuta Maeprasart 5910545019
+          </h4></center>
+          </div>
       </div>
     )
   }
